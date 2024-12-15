c718c772f045dadf3e086e086444985645894f0e
mainHome <38dfoawdf8@gmail.com>
2024-12-15 09:19:54 +0900

    add logging

--- supabase-nextjs/src/app/about/page.tsx
+++ supabase-nextjs/src/app/about/page.tsx
@@ -1,3 +1,10 @@
-export default function About(){
-  return <h1>test</h1>
+import { NextRequest } from 'next/server';
+
+export default function About(req: NextRequest) {
+  console.log(JSON.stringify(req.headers));
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
 }
