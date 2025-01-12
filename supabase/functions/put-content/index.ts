ab84750123314200cf6d5d5fb36205cc5d06c018
mainHome <38dfoawdf8@gmail.com>
2025-01-12 19:27:18 +0900

    f

--- supabase/functions/put-content/index.ts
+++ supabase/functions/put-content/index.ts
@@ -39,7 +39,7 @@ Deno.serve(async (req: Request) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/put-content' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Authorization: Bearer ' \
     --header 'Content-Type: application/json' \
     --data '{"title":"環境に優しい未来へ：再生可能エネルギーの可能性","excerpt":"環境に優しい未来へ：再生可能エネルギーの可能性","content":"test"}'
     
